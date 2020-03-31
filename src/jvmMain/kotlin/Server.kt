d43bf4e0e827bffb55df0e7613e3a16029dcb55f
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2020-03-31 19:24:10 +0200

    URL shenanigans

--- src/jvmMain/kotlin/Server.kt
+++ src/jvmMain/kotlin/Server.kt
@@ -33,7 +33,7 @@ import org.litote.kmongo.eq
 //)
 
 val connectionString: ConnectionString? = System.getenv("MONGODB_URI")?.let {
-    ConnectionString(it + "&retryWrites=false")
+    ConnectionString(it + "?retryWrites=false")
 }
 
 val client =  if(connectionString != null) KMongo.createClient(connectionString) else KMongo.createClient()
