9dc245d341fbf1074050333ea1161a170aa0ccdb
Sebastian Aigner <sebastian.aigner@jetbrains.com>
2020-04-24 16:29:50 +0200

    minimize repro example for IR Ktor Serializer issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,11 @@
-import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpack
-
-val kotlinVersion = "1.3.71"
-val serializationVersion = "0.20.0"
-val ktorVersion = "1.3.2"
+val kotlinVersion = "1.4-M1"
+val serializationVersion = "0.20.0-1.4-M1"
+val ktorVersion = "1.3.2-1.4-M1-2"
 
 plugins {
-    kotlin("multiplatform") version "1.3.71"
+    kotlin("multiplatform") version "1.4-M1"
     application //to run JVM part
-    kotlin("plugin.serialization") version "1.3.70"
+    kotlin("plugin.serialization") version "1.4-M1"
 }
 
 group = "org.example"
@@ -15,6 +13,7 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     maven { setUrl("https://dl.bintray.com/kotlin/kotlin-eap") }
+    maven { setUrl("https://dl.bintray.com/kotlin/kotlin-dev") }
     mavenCentral()
     jcenter()
     maven("https://kotlin.bintray.com/kotlin-js-wrappers/") // react, styled, ...
@@ -25,109 +24,43 @@ kotlin {
     *  To find out how to configure the targets, please follow the link:
     *  https://kotlinlang.org/docs/reference/building-mpp-with-gradle.html#setting-up-targets */
 
-    jvm {
-        withJava()
-    }
     js {
         browser {
             dceTask {
                 keep("ktor-ktor-io.\$\$importsForInline\$\$.ktor-ktor-io.io.ktor.utils.io")
             }
+            produceExecutable()
         }
     }
-sourceSets {
-val commonMain by getting {
-    dependencies {
-        implementation(kotlin("stdlib-common"))
-        implementation("io.ktor:ktor-serialization:$ktorVersion")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:$serializationVersion")
-        implementation("io.ktor:ktor-client-core:$ktorVersion")
-    }
-}
-    val commonTest by getting {
-        dependencies {
-            implementation(kotlin("test-common"))
-            implementation(kotlin("test-annotations-common"))
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+                implementation("io.ktor:ktor-serialization:$ktorVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-common:$serializationVersion")
+                implementation("io.ktor:ktor-client-core:$ktorVersion")
+            }
         }
-    }
 
-val jvmMain by getting {
-    dependencies {
-        implementation("io.ktor:ktor-server-core:$ktorVersion")
-        implementation("io.ktor:ktor-server-netty:$ktorVersion")
-        implementation("ch.qos.logback:logback-classic:1.2.3")
-        implementation(kotlin("stdlib", kotlinVersion)) // or "stdlib-jdk8"
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime:$serializationVersion") // JVM dependency
-        implementation("io.ktor:ktor-websockets:$ktorVersion")
+        val jsMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib-js"))
 
-        implementation("org.litote.kmongo:kmongo-coroutine-serialization:3.12.2")
-    }
-}
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-js:$serializationVersion")
+                //todo: bugfix in kx.serialization?
+                implementation(npm("text-encoding"))
+                implementation(npm("abort-controller"))
 
-    val jsMain by getting {
-        dependencies {
-            implementation(kotlin("stdlib-js"))
+                implementation("io.ktor:ktor-client-js:$ktorVersion")
+                //todo: bugfix in ktor-client?
+                implementation(npm("bufferutil")) //TODO: Uncomment this and stuff breaks. WHY?
+                implementation(npm("utf-8-validate"))
 
-            implementation("org.jetbrains.kotlinx:kotlinx-serialization-runtime-js:$serializationVersion")
-            //todo: bugfix in kx.serialization?
-            implementation(npm("text-encoding"))
-            implementation(npm("abort-controller"))
-
-            implementation("io.ktor:ktor-client-js:$ktorVersion")
-            //todo: bugfix in ktor-client?
-            implementation(npm("bufferutil")) //TODO: Uncomment this and stuff breaks. WHY?
-            implementation(npm("utf-8-validate"))
-
-            //ktor client js json
-            implementation("io.ktor:ktor-client-json-js:$ktorVersion")
-            implementation("io.ktor:ktor-client-serialization-js:$ktorVersion")
-            implementation(npm("fs"))
-
-            //React, React DOM + Wrappers (chapter 3)
-            implementation("org.jetbrains:kotlin-react:16.13.0-pre.93-kotlin-1.3.70")
-            implementation("org.jetbrains:kotlin-react-dom:16.13.0-pre.93-kotlin-1.3.70")
-            implementation(npm("react", "16.13.0"))
-            implementation(npm("react-dom", "16.13.0"))
-        }
-    }
-}
-}
-
-application {
-    mainClassName = "ServerKt"
-}
-
-// include JS artifacts in any JAR we generate
-tasks.getByName<Jar>("jvmJar") {
-    val taskName = if (project.hasProperty("isProduction")) {
-        "jsBrowserProductionWebpack"
-    } else {
-        "jsBrowserDevelopmentWebpack"
-    }
-    val webpackTask = tasks.getByName<KotlinWebpack>(taskName)
-    dependsOn(webpackTask) // make sure JS gets compiled first
-    from(File(webpackTask.destinationDirectory, webpackTask.outputFileName)) // bring output file along into the JAR
-}
-
-
-
-// Alias "installDist" as "stage" for Heroku
-tasks.create("stage") {
-    dependsOn(tasks.getByName("installDist"))
-}
-
-// only necessary until https://youtrack.jetbrains.com/issue/KT-37964 is resolved
-distributions {
-    main {
-        contents {
-            from("$buildDir/libs") {
-                rename("${rootProject.name}-jvm", rootProject.name)
-                into("lib")
+                //ktor client js json
+                implementation("io.ktor:ktor-client-json-js:$ktorVersion")
+                implementation("io.ktor:ktor-client-serialization-js:$ktorVersion")
+                implementation(npm("fs"))
             }
         }
     }
-}
-
-tasks.getByName<JavaExec>("run") {
-    classpath(tasks.getByName<Jar>("jvmJar")) // so that the JS artifacts generated by `jvmJar` can be found and served
 }
--- src/jsMain/kotlin/Api.kt
+++ src/jsMain/kotlin/Api.kt
@@ -1,9 +1,12 @@
-import io.ktor.http.*
-import io.ktor.client.*
-import io.ktor.client.request.*
+import io.ktor.client.HttpClient
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.features.json.serializer.KotlinxSerializer
-
+import io.ktor.client.request.post
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
 import kotlin.browser.window
 
 val endpoint = window.location.origin // only needed until https://github.com/ktorio/ktor/issues/1695 is resolved
@@ -12,17 +15,15 @@ val jsonClient = HttpClient {
     install(JsonFeature) { serializer = KotlinxSerializer() }
 }
 
-suspend fun getShoppingList(): List<ShoppingListItem> {
-    return jsonClient.get(endpoint + ShoppingListItem.path)
-}
+@Serializable
+data class ShoppingListItem(val desc: String, val priority: Int)
 
-suspend fun addShoppingListItem(shoppingListItem: ShoppingListItem) {
-    jsonClient.post<Unit>(endpoint + ShoppingListItem.path) {
-        contentType(ContentType.Application.Json)
-        body = shoppingListItem
+fun main() {
+    val item = ShoppingListItem("Hello", 3)
+    GlobalScope.launch {
+        jsonClient.post<Unit>(endpoint) {
+            contentType(ContentType.Application.Json)
+            body = item
+        }
     }
-}
-
-suspend fun deleteShoppingListItem(shoppingListItem: ShoppingListItem) {
-    jsonClient.delete<Unit>(endpoint + ShoppingListItem.path + "/${shoppingListItem.id}")
 }
